e087397a61bd89b8627f487295988adc30b3bdb1
Xtract01 <meena.balwir149@gmail.com>
2024-12-25 23:46:58 +0530

    first commit

---
+++ src/components/FinishScreen.js
@@ -0,0 +1,36 @@
+function FinishScreen({ points, maxPossiblePoints, highscore, dispatch }) {
+  const percentage = (points / maxPossiblePoints) * 100;
+  let emoji;
+  if (percentage === 100) {
+    emoji = "🥇";
+  }
+  if (percentage >= 80 && percentage < 100) {
+    emoji = "🥈";
+  }
+  if (percentage >= 60 && percentage < 80) {
+    emoji = "🥉";
+  }
+  if (percentage >= 0 && percentage < 60) {
+    emoji = "👎";
+  }
+  if (percentage === 0) {
+    emoji = "😔";
+  }
+  return (
+    <>
+      <p className="result">
+        <span>{emoji}</span> You scores <strong>{points}</strong> out of{" "}
+        {maxPossiblePoints}({Math.ceil(percentage)}%)
+      </p>
+      <p className="highscore">(Highscore: {highscore} points)</p>
+      <button
+        className="btn btn-ui"
+        onClick={() => dispatch({ type: "restart" })}
+      >
+        Restart quiz
+      </button>
+    </>
+  );
+}
+
+export default FinishScreen;
